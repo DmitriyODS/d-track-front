2ca22fdaf42be2614f99b9949045b35e226dbffe
FirendEndlire <demianvlas@gmail.com>
2023-04-11 20:51:51 +0300

    DT-28: Это на Новый Год

--- src/pages/taskBoard/TaskBoard.tsx
+++ src/pages/taskBoard/TaskBoard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './TaskBoard.module.css';
 import { Typography } from '@mui/material';
+import TimelineCards from '../../components/timelineCards/TimelineCards';
 
 class TaskBoard extends React.Component<any, any> {
   render() {
@@ -9,8 +10,25 @@ class TaskBoard extends React.Component<any, any> {
         <div className={styles.title}>
           <Typography variant={'h1'}>Доска задач</Typography>
         </div>
-        <div className={styles.toolbar}></div>
-        <div className={styles.content}></div>
+        <div className={styles.content}>
+          <TimelineCards
+            dataLst={[
+              {
+                itemID: 1,
+                role: 'Мастер',
+                executor: 'Рыбников Артём',
+                date: '20.20.2012',
+                info: 'Сломался упор в плите',
+                creator: 'Угольников Семён Андреевич',
+                number: '17982032-1',
+              },
+            ]}
+            title={'Открыта'}
+          />
+          <TimelineCards dataLst={[]} title={'В работе'} />
+          <TimelineCards dataLst={[]} title={'Оценка'} />
+          <TimelineCards dataLst={[]} title={'Готово'} />
+        </div>
       </div>
     );
   }
