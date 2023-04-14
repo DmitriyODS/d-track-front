62de175e2062856d6f11c4119e4ca1069d6fb8b7
DmitriyODS <osipovskij.dima@yandex.ru>
2023-04-14 17:49:02 +0300

    Правка дизайна (кнопка вернулась на место)

--- src/pages/customers/toolbar/Toolbar.tsx
+++ src/pages/customers/toolbar/Toolbar.tsx
@@ -6,7 +6,6 @@ import IconAdd from '@mui/icons-material/Add';
 import IconOpen from '@mui/icons-material/OpenInNew';
 import IconEdit from '@mui/icons-material/Edit';
 import IconArrow from '@mui/icons-material/Shortcut';
-import IconReplay from '@mui/icons-material/Replay';
 import { ViewModes } from '../../../globals/types';
 import { Collapse, useMediaQuery } from '@mui/material';
 
@@ -19,57 +18,53 @@ function CustomersToolbar({ isSelected }: Props) {
   const matches = useMediaQuery('(min-width: 1650px)');
 
   return (
-    <>
-      <Paper className={styles.root}>
+    <Paper className={styles.root}>
+      <Button
+        className={`${styles.btn} ${styles.btnMini}`}
+        variant={'contained'}
+        color={'secondary'}
+      >
+        <IconAdd />
+        <Collapse in={matches} orientation={'horizontal'}>
+          <p>Добавить</p>
+        </Collapse>
+      </Button>
+      <div className={styles.btnGroup}>
         <Button
-          className={`${styles.btn} ${styles.btnMini}`}
+          className={styles.btn}
           variant={'contained'}
-          color={'secondary'}
-        >
-          <IconAdd />
-          <Collapse in={matches} orientation={'horizontal'}>
-            <p>Добавить</p>
-          </Collapse>
-        </Button>
-        <div className={styles.btnGroup}>
-          <Button
-            className={styles.btn}
-            variant={'contained'}
-            color={'primary'}
-            disabled={!isSelected}
-          >
-            <IconOpen />
-            <Collapse in={matches} orientation={'horizontal'}>
-              <p>Просмотреть</p>
-            </Collapse>
-          </Button>
-          <Button
-            className={styles.btn}
-            variant={'contained'}
-            color={'primary'}
-            disabled={!isSelected}
-          >
-            <IconEdit />
-            <Collapse in={matches} orientation={'horizontal'}>
-              <p>Изменить</p>
-            </Collapse>
-          </Button>
-        </div>
-      </Paper>
-      <Paper className={styles.partTwo}>
-        <Button
-          className={`${styles.btn} ${styles.btnMini}`}
-          variant={'contained'}
-          color={'tertiary'}
+          color={'primary'}
           disabled={!isSelected}
         >
-          <IconArrow />
+          <IconOpen />
           <Collapse in={matches} orientation={'horizontal'}>
-            <p>К заявкам</p>
+            <p>Просмотреть</p>
           </Collapse>
         </Button>
-      </Paper>
-    </>
+        <Button
+          className={styles.btn}
+          variant={'contained'}
+          color={'primary'}
+          disabled={!isSelected}
+        >
+          <IconEdit />
+          <Collapse in={matches} orientation={'horizontal'}>
+            <p>Изменить</p>
+          </Collapse>
+        </Button>
+      </div>
+      <Button
+        className={`${styles.btn} ${styles.btnMini}`}
+        variant={'contained'}
+        color={'tertiary'}
+        disabled={!isSelected}
+      >
+        <IconArrow />
+        <Collapse in={matches} orientation={'horizontal'}>
+          <p>К заявкам</p>
+        </Collapse>
+      </Button>
+    </Paper>
   );
 }
 
